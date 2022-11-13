b484983b795fe2bfebb404a8352cacb539bdac3d
dlwogns <ldyjka@gmail.com>
2022-11-13 04:44:25 +0000

    ~~

--- unsolved/9370.cpp
+++ unsolved/9370.cpp
@@ -5,34 +5,43 @@
 #define INF 2000000001
 using namespace std;
 int T, node_num, edge_num, dest_num, s, g, h;
-pair<vector<int>, vector<int>> ekdlr(int st, vector<vector<pair<int,int>>>&gr){
-    vector<int>dist(node_num+1, INF);
-    vector<int>be(node_num+1, INF);
+pair<vector<double>, vector<int>> ekdlr(int st, vector<vector<pair<int,int>>>&gr){
+    vector<double>dist(node_num+1, INF);
+    vector<int>an(node_num+1, 0);
     dist[st] = 0;
-    priority_queue<pair<int,int>>pq;
+    priority_queue<pair<double,int>>pq;
     pq.push({0,st});
-    be[st] = st;
     while(!pq.empty()){
         int cur_node = pq.top().second;
-        int cur_dist = pq.top().first;
+        double cur_dist = pq.top().first;
         pq.pop();
         for(int i=0; i<gr[cur_node].size(); i++){
             int nxt_node = gr[cur_node][i].first;
-            int nxt_dist = cur_dist + gr[cur_node][i].second;
-            if(dist[nxt_node] == nxt_dist){
-                if(be[nxt_node] == g || be[nxt_node] == h) continue;
-                if(cur_node == g || cur_node == h){
-                    be[nxt_node] = cur_node;
-                }
-            }
-            else if(dist[nxt_node] > nxt_dist){
+            double nxt_dist = cur_dist + gr[cur_node][i].second;
+            // if(dist[nxt_node] == nxt_dist){
+            //     if(an[cur_node] == 1){
+            //         an[nxt_node] = 1;
+            //     }
+            // }
+            if(dist[nxt_node] > nxt_dist){
                 dist[nxt_node] = nxt_dist;
+                if((cur_node == g && nxt_node == h) || (cur_node == h && nxt_node == g))
+                    dist[nxt_node] += 0.1;
                 pq.push({nxt_dist, nxt_node});
-                be[nxt_node] = cur_node;
+                // if(cur_node == h && nxt_node == g){
+                //     an[nxt_node] = 1;
+                //     continue;
+                // }
+                // if(cur_node == g && nxt_node == h){
+                //     an[nxt_node] = 1;
+                //     continue;
+                // }
+                // if(an[cur_node] == 1)an[nxt_node] = 1;
+                // if(an[cur_node] == 0) an[nxt_node] = 0;
             }
         }
     }
-    return {dist, be};
+    return {dist, an};
 }
 int main(){
     ios::sync_with_stdio(0);
@@ -57,14 +66,8 @@ int main(){
         vector<int>ans;
         for(auto d : dest){
             if(check.first[d] == INF) continue;
-            int c = d;
-            while(check.second[c] != c){
-                if((check.second[c] == h && c == g) || (check.second[c]==g && c == h)){
-                    ans.push_back(d);
-                    break;
-                }
-                c = check.second[c];
-            }
+            if(int(check.first[d]) != check.first[d]) ans.push_back(d);
+            //if(check.second[d]) ans.push_back(d);
         }
         sort(ans.begin(), ans.end());
         for(int i=0; i<ans.size(); i++)
