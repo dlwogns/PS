813bf9011ed1a57bd3ef97e85114da5f39a65467
dlwogns <ldyjka@gmail.com>
2023-02-11 23:42:03 +0900

    move dir

---
+++ sef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+int N;
+vector<int>arr;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin>>N;
+    for(int i=0; i<N; i++){
+        int input;
+        cin>>input;
+        arr.push_back(input);
+        
+    }
+}
