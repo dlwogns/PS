3663686031d3b988719e0f06d017eb1ede1e8cb8
leejaehun <ldyjka@gmail.com>
2023-06-29 16:04:43 +0900

    sdf

--- 과외용/2.cpp
+++ 과외용/2.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+class Node{
+    public:
+        Node* next;
+};
+
+// pointer
+// call by value, call by reference
+int func(int a, int b){
+    //call by value
+}
+int func2(int& a, int& b){
+    //call by reference
+}
+// value, reference
+
 int main(){
-    int N;
-    cin>>N;
-    vector<int>v;
-    for(int i=0; i<N; i++){
-        int input;
-        cin>>input;
-        v.push_back(input);
-    }
-    x
+    int a = 1;
+    int* pa = &a;
+    
+    // & -> reference, * -> pointer
+    // & -> 어떤 변수의 주소를 의미. (참조) 연산자
+    cout<<&a<<endl;
+    // * -> 역참조, 주소에 해당하는 값 연산자
+    cout<<*pa<<endl;
+    // 자료형* -> 포인터 변수 자료형(주소값을 저장)
+    int* pa2 = &a;
+
+
 }
